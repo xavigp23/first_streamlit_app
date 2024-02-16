963e7452b2059ca2da9c91d2353f64228981a92d
xavigp23 <79249184+xavigp23@users.noreply.github.com>
2024-02-16 14:34:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,11 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like information about?')
+streamlit.write("thanks for adding", add_my_fruit)
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')" )
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
