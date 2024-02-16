c3d3a94c3ef5b9e239aa5b692709e9d548e41235
xavigp23 <79249184+xavigp23@users.noreply.github.com>
2024-02-16 11:19:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,10 @@ streamlit.title('My Parents My healthy diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega3')
-streamlit.text('Spinach')
-streamlit.text('Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
