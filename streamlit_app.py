86ed3da544f451621ec7621b1a89785f4485f696
xavigp23 <79249184+xavigp23@users.noreply.github.com>
2024-02-16 11:16:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents My healthy diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega3')
+streamlit.text('Spinach')
+streamlit.text('Egg')
+
+
